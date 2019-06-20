530e1f6e75f7697e70ea575d3acfeca1100937d3
Steven Miller <sjmiller609@gmail.com>
2019-06-20 19:09:29 +0000

    support for nlb lookup

--- elb_lookup.tf
+++ elb_lookup.tf
@@ -5,10 +5,14 @@ appropriate terraform data source
 does not exist (look up ELB by vpc / tags)
 */
 
+resource random_id "collision_avoidance" {
+  byte_length = 5
+}
+
 # This data source will be used in other files
 resource "aws_iam_role_policy" "elb_lookup_policy" {
 
-  name = "${var.deployment_id}_elb_lookup_policy"
+  name = "${var.deployment_id}_elb_lookup_policy${random_id.collision_avoidance.hex}"
   role = "${aws_iam_role.elb_lookup_role.id}"
 
   policy = <<EOF
@@ -25,7 +29,7 @@ EOF
 }
 
 resource "aws_iam_role" "elb_lookup_role" {
-  name = "${var.deployment_id}_elb_lookup_role"
+  name = "${var.deployment_id}_elb_lookup_role${random_id.collision_avoidance.hex}"
 
   assume_role_policy = <<EOF
 {
@@ -53,7 +57,7 @@ data "archive_file" "elb_lookup" {
 resource "aws_lambda_function" "elb_lookup" {
   depends_on       = ["data.archive_file.elb_lookup"]
   filename         = "${path.module}/elb_lookup.py.zip"
-  function_name    = "${var.deployment_id}_elb_lookup_function"
+  function_name    = "${var.deployment_id}_elb_lookup_function${random_id.collision_avoidance.hex}"
   role             = "${aws_iam_role.elb_lookup_role.arn}"
   handler          = "elb_lookup.my_handler"
   source_code_hash = "${data.archive_file.elb_lookup.output_base64sha256}"
--- files/elb_lookup.py
+++ files/elb_lookup.py
@@ -2,12 +2,15 @@ import os
 import boto3
 
 client = boto3.client('elb')
+clientv2 = boto3.client('elbv2')
 vpc_id = os.environ['VPC_ID']
 cluster_name = os.environ['CLUSTER_NAME']
 
 def _get_elb_name():
     response = client.describe_load_balancers()
+    response2 = clientv2.describe_load_balancers()
     load_balancers = response['LoadBalancerDescriptions']
+    nlb_load_balancers = response2['LoadBalancers']
 
     # get all the ELBs in this VPC
     load_balancer_names_in_vpc = []
@@ -15,14 +18,39 @@ def _get_elb_name():
         if lb['VPCId'] == vpc_id:
             load_balancer_names_in_vpc.append(lb["LoadBalancerName"])
 
+    # get all the ELBs in this VPC
+    nlb_load_balancer_arns_in_vpc = []
+    for lb in nlb_load_balancers:
+        if lb['VpcId'] == vpc_id:
+            nlb_load_balancer_arns_in_vpc.append(lb["LoadBalancerArn"])
+
+    tag_descriptions = []
     # get the tags
-    response = client.describe_tags(
-        LoadBalancerNames=load_balancer_names_in_vpc)
+    if len(load_balancer_names_in_vpc) > 0:
+        response = client.describe_tags(
+            LoadBalancerNames=load_balancer_names_in_vpc)
+        tag_descriptions += response['TagDescriptions']
+    if len(nlb_load_balancer_arns_in_vpc) > 0:
+        response2 = clientv2.describe_tags(
+            ResourceArns=nlb_load_balancer_arns_in_vpc)
+        tag_descriptions += response2['TagDescriptions']
 
     # return the name of the ELB corresponding to this cluster
-    for description in response['TagDescriptions']:
+    for description in tag_descriptions:
+        tags = {}
         for tag in description['Tags']:
-            if tag['Key'] == f"kubernetes.io/cluster/{cluster_name}":
+            tags[tag['Key']] = tag['Value']
+        if f"kubernetes.io/cluster/{cluster_name}" in tags.keys() \
+          and "kubernetes.io/service-name" in tags.keys() \
+          and tags["kubernetes.io/service-name"] == "astronomer/astronomer-nginx":
+            if 'ResourceArn' in description.keys():
+                arn = description['ResourceArn']
+                for lb in nlb_load_balancers:
+                    if lb['LoadBalancerArn'] == arn:
+                        return {
+                            "Name" : lb['LoadBalancerName']
+                        }
+            else:
                 return {
                     "Name" : description['LoadBalancerName']
                 }
