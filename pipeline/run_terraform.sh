9984d3ed51d0c420dba8e64bd8fc7518c467429a
Ian Stanton <ian@astronomer.io>
2020-12-14 11:27:14 -0600

    Update run_terraform script

--- pipeline/run_terraform.sh
+++ pipeline/run_terraform.sh
@@ -1,6 +1,6 @@
 set -xe
 
-terraform -v
+terraform-0.12.29 -v
 
 # unique deployment ID to avoid collisions in CI
 # needs to be 32 characters or less and start with letter
@@ -12,12 +12,12 @@ cp backend.tf.example examples/$EXAMPLE/backend.tf
 cd examples/$EXAMPLE
 sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
 
-terraform init
+terraform-0.12.29 init
 
 if [ $DESTROY -eq 1 ]; then
-  terraform destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  terraform-0.12.29 destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
 else
-  terraform apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  terraform-0.12.29 apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
   # check that kubernetes is up and running
   export KUBECONFIG=./kubeconfig-$DEPLOYMENT_ID
   kubectl get namespaces
