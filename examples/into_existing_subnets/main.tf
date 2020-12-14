cf1c469f5003d8438be0d5fab404a6b1d0cc9a68
Ian Stanton <ian@astronomer.io>
2020-12-14 16:43:00 -0600

    Change region to us-west-2

--- examples/into_existing_subnets/main.tf
+++ examples/into_existing_subnets/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
