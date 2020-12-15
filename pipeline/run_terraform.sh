48a90a5237b6b0278c0cda454327e5e59b7469cb
Ian Stanton <ian@astronomer.io>
2020-12-15 12:05:54 -0600

    Add terraform command var

--- pipeline/run_terraform.sh
+++ pipeline/run_terraform.sh
@@ -1,6 +1,8 @@
 set -xe
 
-terraform-0.12.29 -v
+TERRAFORM=terraform-0.12.29
+
+$TERRAFORM -v
 
 # unique deployment ID to avoid collisions in CI
 # needs to be 32 characters or less and start with letter
@@ -13,14 +15,14 @@ if [ $DESTROY -eq 1 ]; then
   DEPLOYMENT_ID=ci$(echo "$CIRCLE_PROJECT_REPONAME$CIRCLE_PREVIOUS_BUILD_NUM" | md5sum | awk '{print substr($1,0,30)}')
   echo $DEPLOYMENT_ID
   sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
-  terraform-0.12.29 init
-  terraform-0.12.29 destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  $TERRAFORM init
+  $TERRAFORM destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
 else
   DEPLOYMENT_ID=ci$(echo "$CIRCLE_PROJECT_REPONAME$CIRCLE_BUILD_NUM" | md5sum | awk '{print substr($1,0,30)}')
   echo $DEPLOYMENT_ID
   sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
-  terraform-0.12.29 init
-  terraform-0.12.29 apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  $TERRAFORM init
+  $TERRAFORM apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
   # check that kubernetes is up and running
   export KUBECONFIG=./kubeconfig-$DEPLOYMENT_ID
   kubectl get namespaces
