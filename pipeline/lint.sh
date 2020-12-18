64b7f39ab4446e0990cfa93274529f3f1062f089
Ian Stanton <ian@astronomer.io>
2020-12-18 08:30:51 -0600

    Add subshell

--- pipeline/lint.sh
+++ pipeline/lint.sh
@@ -11,10 +11,11 @@ $TERRAFORM fmt -check=true
 $TERRAFORM validate -var "deployment_id=validate" -var "route53_domain=validate-fake.com" -var "admin_email=fake@mailinator.com"
 for example in $(find examples -maxdepth 1 -mindepth 1 -type d); do
   cp providers.tf "$example"
-  cd "$example"
-  echo "$example"
-  $TERRAFORM init
-  $TERRAFORM fmt -check=true
-  $TERRAFORM validate -var "deployment_id=citest"
-  cd -
+  (
+    cd "$example"
+    echo "$example"
+    $TERRAFORM init
+    $TERRAFORM fmt -check=true
+    $TERRAFORM validate -var "deployment_id=citest"
+  )
 done
