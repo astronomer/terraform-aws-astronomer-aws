f41e10a835af7005ab0c96034e75263d0c1c1270
Ian Stanton <ian@astronomer.io>
2020-12-18 13:36:11 -0600

    Migrate CI Pipeline to CircleCI (#51)
    * Migrate CI pipeline from Drone to CircleCI
    * Remove `.drone.yml`
    
    Co-authored-by: Daniel <danielh@astronomer.io>

---
+++ pipeline/lint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# shellcheck disable=SC2044
+set -xe
+
+TERRAFORM="${TERRAFORM:-terraform-0.12.29}"
+
+$TERRAFORM -v
+
+cp providers.tf.example providers.tf
+$TERRAFORM init
+$TERRAFORM fmt -check=true
+$TERRAFORM validate -var "deployment_id=validate" -var "route53_domain=validate-fake.com" -var "admin_email=fake@mailinator.com"
+for example in $(find examples -maxdepth 1 -mindepth 1 -type d); do
+  cp providers.tf "$example"
+  (
+    cd "$example"
+    echo "$example"
+    $TERRAFORM init
+    $TERRAFORM fmt -check=true
+    $TERRAFORM validate -var "deployment_id=citest"
+  )
+done
--- pipeline/run_terraform.sh
+++ pipeline/run_terraform.sh
@@ -1,23 +1,26 @@
+#!/bin/bash
 set -xe
 
-terraform -v
+TERRAFORM="${TERRAFORM:-terraform-0.12.29}"
 
-# unique deployment ID to avoid collisions in CI
-# needs to be 32 characters or less and start with letter
-DEPLOYMENT_ID=ci$(echo "$DRONE_REPO_NAME$DRONE_BUILD_NUMBER" | md5sum | awk '{print substr($1,0,30)}')
-echo $DEPLOYMENT_ID
+$TERRAFORM -v
 
-cp providers.tf.example examples/$EXAMPLE/providers.tf
-cp backend.tf.example examples/$EXAMPLE/backend.tf
-cd examples/$EXAMPLE
-sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
+cp providers.tf.example examples/"$EXAMPLE"/providers.tf
+cp backend.tf.example examples/"$EXAMPLE"/backend.tf
+cd examples/"$EXAMPLE"
 
-terraform init
-
-if [ $DESTROY -eq 1 ]; then
-  terraform destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+if [ "$DESTROY" -eq 1 ]; then
+  DEPLOYMENT_ID=ci$(echo "$CIRCLE_PROJECT_REPONAME$CIRCLE_PREVIOUS_BUILD_NUM" | md5sum | awk '{print substr($1,0,30)}')
+  echo "$DEPLOYMENT_ID"
+  sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
+  $TERRAFORM init
+  $TERRAFORM destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
 else
-  terraform apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  DEPLOYMENT_ID=ci$(echo "$CIRCLE_PROJECT_REPONAME$CIRCLE_BUILD_NUM" | md5sum | awk '{print substr($1,0,30)}')
+  echo "$DEPLOYMENT_ID"
+  sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
+  $TERRAFORM init
+  $TERRAFORM apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
   # check that kubernetes is up and running
   export KUBECONFIG=./kubeconfig-$DEPLOYMENT_ID
   kubectl get namespaces
